4a268206bc1a082ea029ab3a6dcb0dfd74d1afe0
Leif Warner <abimelech@gmail.com>
2011-06-09 14:27:50 -0700

    Working on adding pre-query bindings to SPARQL queries.

--- src/main/scala/org/scardf/jena/JenaGraphPlus.scala
+++ src/main/scala/org/scardf/jena/JenaGraphPlus.scala
@@ -2,13 +2,17 @@ package org.scardf.jena
 import org.scardf.{Node, UriRef, SubjectNode, RdfTriple, RDF}
 import com.hp.hpl.jena.rdf.model.Model
 
-class JenaGraphPlus(models:Model*) extends JenaGraph(models.head) {
+class JenaGraphPlus(bindings:Map[String,String], models:Model*) extends JenaGraph(models.head) {
+    def this(models:Model*) = this(Map.empty:Map[String,String], models:_*)
     def resourcesWithProperty(p:UriRef, o:Node) = new JenaResIterator( m.listResourcesWithProperty(property(p), rdfnode(o)) )
     def instancesOf(o:UriRef) = resourcesWithProperty(RDF.Type, o)
 
     def contains( s: SubjectNode, p:UriRef ) = m.contains( resource( s ), property( p ) )
 
     def remove( t: RdfTriple ) = m remove statement( t )
+
+    def binding(elems: (String, String)*) = new JenaGraphPlus(Map(elems:_*), models:_*)
+
     def close = models.foreach(_.close)
 
     def read(file:String) = m.read("file:"+file, "TURTLE")
